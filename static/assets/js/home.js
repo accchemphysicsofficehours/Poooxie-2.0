daaef0568c463dfa1eab038e05e2aadceb6465da
accalgebraofficehours <147888375+accalgebraofficehours@users.noreply.github.com>
2025-03-19 23:42:44 -0600

    Update home.js

--- static/assets/js/home.js
+++ static/assets/js/home.js
@@ -174,16 +174,7 @@ document.addEventListener("DOMContentLoaded", event => {
 });
 // Splash texts
 const SplashT = [
-  "Over 8 Million Users since 2023",
-  "Fastest growing proxy server",
-  "Made by xBubbo",
-  "Check out discord.gg/interstellar :)",
-  "Thanks for using the site",
-  "Follow us on Tiktok (@useinterstellar)",
-  "Subscribe to us on YouTube (@unblocking)",
-  "Subscribe to my Youtube (@xbubbo)",
-  "Check out the settings page",
-  "Check out our Patreon (https://www.patreon.com/gointerstellar)",
+  "poodleschool.net is a fun website!",
 ];
 
 let SplashI = Math.floor(Math.random() * SplashT.length);
@@ -200,19 +191,7 @@ SplashE.addEventListener("click", US);
 // Random URL
 function getRandomUrl() {
   const randomUrls = [
-    "https://kahoot.it",
-    "https://classroom.google.com",
-    "https://drive.google.com",
-    "https://google.com",
-    "https://docs.google.com",
-    "https://slides.google.com",
-    "https://www.nasa.gov",
-    "https://blooket.com",
-    "https://clever.com",
-    "https://edpuzzle.com",
-    "https://khanacademy.org",
-    "https://wikipedia.org",
-    "https://dictionary.com",
+    "https://poodleschool.net",
   ];
   return randomUrls[randRange(0, randomUrls.length)];
 }
